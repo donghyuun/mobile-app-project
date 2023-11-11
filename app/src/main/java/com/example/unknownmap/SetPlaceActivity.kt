9f337d8793f44c867589a8d00078e8538b5c564f
donghyuun <bomul0524@gmail.com>
2023-11-11 14:13:07 +0900

    ë기존의 로그인 기능에 ì장소ë준혁님의 기능 합침

---
+++ app/src/main/java/com/example/unknownmap/SetPlaceActivity.kt
@@ -0,0 +1,177 @@
+package com.example.unknownmap
+
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.Toolbar
+import com.example.unknownmap.databinding.ActivityMainBinding
+import com.example.unknownmap.databinding.ActivitySetPlaceBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
+import java.net.URI
+import java.util.Collections
+
+class SetPlaceActivity : AppCompatActivity() {
+    var isSet : Boolean = false
+
+    // 사진 업로드를 위한 Activity에서 결과 가져오기
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySetPlaceBinding.inflate(layoutInflater)
+
+        val latitude = intent.getDoubleExtra("create_latitude", 0.0)
+        val longitude = intent.getDoubleExtra("create_longitude", 0.0)
+
+        // 지금 설정된 카테고리 번호
+        var currentSelectedNum = 0
+
+        Log.d("kim", "first got ${latitude}, ${longitude}")
+
+        // 툴바 뒤로 가기
+        binding.setPlaceBackBtn.setOnClickListener{
+            finish()
+        }
+
+        val btnTrashBin = binding.categoryTrashBin
+        val btnVendingMachine = binding.categoryVendingMachine
+        val btnFish = binding.categoryFish
+        val btnClothesDonation = binding.categoryClothesDonation
+        val btnPullUpBar = binding.categoryPullUpBar
+        val btnCigar = binding.categoryCigar
+
+        val btnList : List<ImageView> = listOf<ImageView>(btnTrashBin, btnVendingMachine, btnFish, btnClothesDonation, btnPullUpBar, btnCigar)
+
+        // 카테고리 버튼 클릭 시, 색깔 활성화 및 현재 눌려진 버튼 확인
+        binding.categoryTrashBin.setOnClickListener{
+            btnTrashBin.setColorFilter(Color.parseColor("#00000000"))
+            btnTrashBin.setBackgroundResource(R.drawable.corner_round)
+            currentSelectedNum = 0
+            for (i in btnList) {
+                if (i != btnTrashBin){
+                    i.setColorFilter(Color.parseColor("#af000000"))
+                    i.setBackgroundResource(R.drawable.transparent)
+                }
+            }
+        }
+
+        binding.categoryVendingMachine.setOnClickListener{
+            btnVendingMachine.setColorFilter(Color.parseColor("#00000000"))
+            btnVendingMachine.setBackgroundResource(R.drawable.corner_round)
+            currentSelectedNum = 1
+            for (i in btnList) {
+                if (i != btnVendingMachine) {
+                    i.setColorFilter(Color.parseColor("#af000000"))
+                    i.setBackgroundResource(R.drawable.transparent)
+                }
+            }
+        }
+
+        binding.categoryFish.setOnClickListener{
+            btnFish.setColorFilter(Color.parseColor("#00000000"))
+            btnFish.setBackgroundResource(R.drawable.corner_round)
+            currentSelectedNum = 2
+            btnFish.setColorFilter(Color.parseColor("#00000000"))
+            for (i in btnList) {
+                if (i != btnFish) {
+                    i.setColorFilter(Color.parseColor("#af000000"))
+                    i.setBackgroundResource(R.drawable.transparent)
+                }
+            }
+        }
+
+        binding.categoryClothesDonation.setOnClickListener{
+            btnClothesDonation.setColorFilter(Color.parseColor("#00000000"))
+            btnClothesDonation.setBackgroundResource(R.drawable.corner_round)
+            currentSelectedNum = 3
+            btnClothesDonation.setColorFilter(Color.parseColor("#00000000"))
+            for (i in btnList) {
+                if (i != btnClothesDonation) {
+                    i.setColorFilter(Color.parseColor("#af000000"))
+                    i.setBackgroundResource(R.drawable.transparent)
+                }
+            }
+        }
+
+        binding.categoryPullUpBar.setOnClickListener{
+            btnPullUpBar.setColorFilter(Color.parseColor("#00000000"))
+            btnPullUpBar.setBackgroundResource(R.drawable.corner_round)
+            currentSelectedNum = 4
+            btnPullUpBar.setColorFilter(Color.parseColor("#00000000"))
+            for (i in btnList) {
+                if (i != btnPullUpBar) {
+                    i.setColorFilter(Color.parseColor("#af000000"))
+                    i.setBackgroundResource(R.drawable.transparent)
+                }
+            }
+        }
+
+        binding.categoryCigar.setOnClickListener{
+            btnCigar.setColorFilter(Color.parseColor("#00000000"))
+            btnCigar.setBackgroundResource(R.drawable.corner_round)
+            currentSelectedNum = 5
+            btnCigar.setColorFilter(Color.parseColor("#00000000"))
+            for (i in btnList) {
+                if (i != btnCigar) {
+                    i.setColorFilter(Color.parseColor("#af000000"))
+                    i.setBackgroundResource(R.drawable.transparent)
+                }
+            }
+        }
+
+        var uri : Uri? = null
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){ result ->
+            // 이미지 받아와서 보여주기
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                uri = result.data!!.data!!
+
+                binding.setPlaceShowImage.setImageURI(uri)
+            }
+        }
+        // 이미지 등록 버튼 리스너
+        binding.setPlaceSetImageBtn.setOnClickListener {
+            // 이미지 추가를 위한 코드 시작
+            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            val chooser = Intent.createChooser(galleryIntent, "이미지 등록하기")
+            chooser.putExtra(
+                Intent.EXTRA_INITIAL_INTENTS,
+                arrayOf(cameraIntent)
+            )
+            resultLauncher.launch(chooser)
+        }
+
+        // 등록 버튼 리스너
+        binding.setPlaceSetBtn.setOnClickListener {
+            val name = binding.placeName.text.toString()
+
+            intent.putExtra("set_latitude", latitude)
+            intent.putExtra("set_longitude", longitude)
+            intent.putExtra("set_name", name)
+
+            intent.putExtra("isSet", true)
+            intent.putExtra("categoryNum", currentSelectedNum)
+            intent.putExtra("image", uri.toString())
+            // uri는 String으로 변환해서 intent로 넘기고, 받을 때 다시 parse 해야 함
+
+            setResult(RESULT_OK, intent)
+
+            Log.d("kim", "$latitude, $longitude, $name, $currentSelectedNum transferred to MainActivity")
+            finish()
+        }
+
+        setContentView(binding.root)
+    }
+
+}
