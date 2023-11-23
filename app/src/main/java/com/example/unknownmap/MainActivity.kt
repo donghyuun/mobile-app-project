08a3183c094a2f970f3f22edd593f41cd3cd9225
donghyuun <bomul0524@gmail.com>
2023-11-24 02:13:44 +0900

    ãadd mypage & small detail

--- app/src/main/java/com/example/unknownmap/MainActivity.kt
+++ app/src/main/java/com/example/unknownmap/MainActivity.kt
@@ -157,9 +157,11 @@ class MainActivity : AppCompatActivity(), MapView.POIItemEventListener, MapView.
         // 추출한 데이터를 사용
         Log.d("LOGIN", "In MainActivity, User ID: $userId, Email: $userEmail, Nickname: $userNickname, Token: $userToken")
 
-        //----------------------카카오 로그아웃 버튼------------------------//
+        //----------------------마이페이지 이동 버튼------------------------//
         binding.mainMypageBtn.setOnClickListener{
-            kakaoLogout()
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+//            kakaoLogout()
         }
 
         val mapView = MapView(this)
@@ -527,52 +529,6 @@ class MainActivity : AppCompatActivity(), MapView.POIItemEventListener, MapView.
     override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {}
 
     // MapViewEventListener Interface end
-
-    // KAKAO 로그인 코드 start
-    fun kakaoLogout(){
-        // 로그아웃 다이얼로그 생성
-        val dialogBuilder = AlertDialog.Builder(this)
-        dialogBuilder.setMessage("정말 로그아웃 하시겠습니까?")
-            .setCancelable(false)
-            .setPositiveButton("확인", DialogInterface.OnClickListener {
-                    dialog, id -> confirmLogout()
-            })
-            .setNegativeButton("취소", DialogInterface.OnClickListener {
-                    dialog, id -> dialog.cancel()
-            })
-
-        val alert = dialogBuilder.create()
-        alert.setTitle("로그아웃")
-        alert.show()
-    }
-
-    private fun confirmLogout(){
-        //로그아웃
-        UserApiClient.instance.logout { error ->
-            if (error != null) {
-                Log.e("LOGOUT", "로그아웃 실패. SDK에서 토큰 삭제됨", error)
-            } else {
-                Log.i("LOGOUT", "로그아웃 성공. SDK에서 토큰 삭제됨")
-                val intent = Intent(this, LoginActivity::class.java)
-                //스택에 남아있는 모든 액티비티를 제거하고, 해당 엑티비티를 시작함
-                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                //현재 액티비티를 종료
-                finish()
-            }
-        }
-    }
-    fun kakaoUnlink() {
-        //연결 끊기
-        UserApiClient.instance.unlink { error ->
-            if (error != null) {
-                Log.e("LOGOUT", "연결 끊기 실패", error)
-            } else {
-                Log.i("LOGOUT", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
-            }
-        }
-        finish()
-    }
-    // 카카오 로그인 코드 end
 
     //------------------생명 주기 확인--------------------//
     override fun onStart() {
