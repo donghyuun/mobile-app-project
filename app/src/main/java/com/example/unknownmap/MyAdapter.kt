1ca2991d3cf28c4ba237bddefa408f1532f032d9
donghyuun <bomul0524@gmail.com>
2023-12-01 23:01:58 +0900

    add review showing func

--- app/src/main/java/com/example/unknownmap/MyAdapter.kt
+++ app/src/main/java/com/example/unknownmap/MyAdapter.kt
@@ -5,6 +5,8 @@
     import android.view.ViewGroup
     import androidx.recyclerview.widget.RecyclerView
     import com.example.unknownmap.databinding.CommentItemBinding
+    import java.security.Timestamp
+    import java.util.Objects
 
     class MyAdapter(val reviewList:MutableList<KeyValueElement>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         class MyViewHolder(val binding: CommentItemBinding): RecyclerView.ViewHolder(binding.root)//항목을 구성하는 view객체
@@ -19,13 +21,42 @@
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//            if (position < reviewList.size) {
-//                Log.d("review", "onBindView ${reviewList[position]}")
-//                val hsMap = reviewList[position]
-//                Log.d("review", "hsMap: onBindView ${hsMap}")
-//                // 뷰에 데이터 출력
-//            } else {
-//                Log.e("review", "Invalid position: $position")
-//            }
+            Log.d("review", "MyAdapter: onBindViewHolder: ${reviewList[position]}")
+            Log.d("review", "MyAdapter: onBindViewHolder reviewList.size: ${reviewList.size}")
+            Log.d("review", "MyAdapter: onBindViewHolder position: ${position}")
+            Log.d("review", "MyAdapter: onBindViewHolder typeof reviewList[position]: ${reviewList[position]::class.java}")
+
+            data class ReviewData(
+                val createdDate: com.google.firebase.Timestamp? = null,
+                val userNickName: String? = null,
+                val userId: String? = null,
+                val content: String? = null
+            )
+
+            try {
+                if (reviewList != null && position >= 0 && position < reviewList.size) {
+                    val dataFromFirestore: HashMap<String, Any> = reviewList[position] as HashMap<String, Any>
+                    for ((key, value) in dataFromFirestore) {
+                        Log.d("review","Key: $key, Value: $value")
+                    }
+                    val reviewData = ReviewData(
+                        createdDate = dataFromFirestore["createdDate"] as? com.google.firebase.Timestamp,
+                        userNickName = dataFromFirestore["userNickName"] as String?,
+                        userId = dataFromFirestore["userId"] as String?,
+                        content = dataFromFirestore["content"] as String?
+                    )
+                    val binding = (holder as MyViewHolder).binding
+                    binding.commentUserId.text = reviewData.userNickName
+                    binding.commentContent.text = reviewData.content
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.d("review", "MyAdapter: onBindViewHolder error: ${e}")
+            }
+            Log.d("review", "MyAdapter: onBindViewHolder end position")
+//            Log.d("review", "MyAdapter: onBindViewHolder content: ${content}")
+//            Log.d("review", "MyAdapter: onBindViewHolder reviewList[position].content: ${reviewList[position].get("content")}")
+//            Log.d("MyAdapter", "onBindViewHolder: ${reviewList[position].get("content")}")
+
         }
     }
--- app/src/main/java/com/example/unknownmap/ShowPlaceActivity.kt
+++ app/src/main/java/com/example/unknownmap/ShowPlaceActivity.kt
@@ -183,23 +183,19 @@ class ShowPlaceActivity : AppCompatActivity() {
 
                     //여기서 리사이클러뷰에 리뷰 목록을 출력하도록 해야함, 안그러면 DB에서 리뷰 목록 받아오는 것보다 더 빨리 실행되서 리뷰가 정상적으로 출력되지 않음
                     val layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)//한줄에 항목을 몇개 배치할지
-                    Log.d("review", "E")
                     binding.commentsRecyclerView.layoutManager = layoutManager
-                    Log.d("review", "F")
-                    Log.d("review", "G: ${reviewList.toString()}")
-                    Log.d("review", "G: ${reviewList.size.toString()}")
+                    Log.d("review", "ShowPlaceActivity: reviewList.toString: ${reviewList.toString()}")
+                    Log.d("review", "ShowPlaceActivity: reviewList.size.toString: ${reviewList.size.toString()}")
                     binding.commentsRecyclerView.adapter = MyAdapter(reviewList)
-                    Log.d("review", "H")
-
                 } else{
-                    Log.d("DB", "review list does not exist")
+                    Log.d("review", "ShowPlaceActivity: review list does not exist")
                 }
             }
             .addOnFailureListener { exception ->
-                Log.d("DB", "get failed with ", exception)
+                Log.d("review", "ShowPlaceActivity: get failed with ", exception)
             }
 
-        Log.d("DB","adfasdfadsfaf")
+        Log.d("review","adfasdfadsfaf")
     }
 
     private fun getCategoryString(category: Int): String {
